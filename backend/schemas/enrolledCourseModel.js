478aaf2012b731fbed08aca196e1e9f28a4bd7d8
kpraneetha22 <praneethakramadhati@gmail.com>
2025-06-26 23:02:33 +0530

    Initial commit

---
+++ backend/schemas/enrolledCourseModel.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const enrolledCourseSchema = mongoose.Schema(
+  {
+    courseId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "course",
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+    },
+    course_Length: {
+      type: Number,
+      required: true,
+    },
+    progress: [
+  {
+    sectionId: String, // or ObjectId if referencing
+    completedAt: Date, // optional
+  }
+],
+    certificateDate: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("enrolledCourses", enrolledCourseSchema);
